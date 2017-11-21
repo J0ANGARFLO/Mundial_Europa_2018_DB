9582ae8e91852966985a399cd21a6fba131e0c3e
danielrparra77 <danielrparra77@hotmail.com>
2017-11-20 22:58:55 -0500

    quinta entrega aun incompleta
    quinta entrega aun incompleta

--- database/Mundial Europa.sql
+++ database/Mundial Europa.sql
@@ -4,6 +4,10 @@
 */
 
 -- Eliminacion de tablas en caso de que existan
+DROP TABLE IF EXISTS Jugadores;
+DROP TABLE IF EXISTS Dirige;
+DROP TABLE IF EXISTS Participantes;
+DROP TABLE IF EXISTS Equipos;
 DROP TABLE IF EXISTS DirectoresTecnicos;
 DROP TABLE IF EXISTS Arbitros;
 DROP TABLE IF EXISTS Personas;
@@ -136,3 +140,72 @@ create table DirectoresTecnicos(
 );
 CREATE INDEX IXFK_ADirectoresTecnicos_Personas ON DirectoresTecnicos(cod_persona);
 CREATE INDEX IXFK_ADirectoresTecnicos_Pais ON DirectoresTecnicos(cod_pais);
+
+/**
+Creación de la tabla de los equipos
+*/
+create table Equipos(
+	cod_equipo varchar(4),
+	nomb_equipo varchar(50) not null,
+	ciudad_sede varchar (5) not null,
+	cod_confe smallint not null,
+	
+	CONSTRAINT FK_Equipos_Ciudades FOREIGN KEY (ciudad_sede) REFERENCES Ciudades (cod_ciudad),
+	CONSTRAINT FK_Equipos_Confederaciones FOREIGN KEY (cod_confe) REFERENCES Confederaciones (cod_confe),
+	CONSTRAINT PK_Equipos PRIMARY KEY(cod_equipo)
+);
+CREATE INDEX IXFK_Equipos_Ciudades ON Equipos(ciudad_sede);
+CREATE INDEX IXFK_Equipos_Confederaciones ON Equipos(cod_confe);
+
+/**
+Creación de la tabla de los equipos participantes en los mundiales
+*/
+create table Participantes(
+	anio_mundial smallint,
+	cod_equipo varchar(4) not null,
+	puesto smallint not null,
+	
+	CONSTRAINT FK_Participantes_Equipos FOREIGN KEY (cod_equipo) REFERENCES Equipos (cod_equipo),
+	CONSTRAINT FK_Participantes_Mundiales FOREIGN KEY (anio_mundial) REFERENCES Torneos (anio),
+	CONSTRAINT PK_Participantes PRIMARY KEY(anio_mundial,cod_equipo)
+);
+CREATE INDEX IXFK_Participantes_Equipos ON Participantes(cod_equipo);
+CREATE INDEX IXFK_Participantes_Mundiales ON Participantes(anio_mundial);
+
+/**
+Creación de la tabla de los directores que dirigen un equipo
+*/
+create table Dirige(
+	cod_equipo varchar(4) not null,
+	cod_dt decimal(5) not null,
+	fecha_inicio date not null,
+	fecha_fin date,
+	
+	CONSTRAINT FK_Dirige_DirectoresTecnicos FOREIGN KEY (cod_dt) REFERENCES DirectoresTecnicos (cod_dt),
+	CONSTRAINT FK_Dirige_Equipos FOREIGN KEY (cod_equipo) REFERENCES Equipos (cod_equipo),
+	CONSTRAINT PK_Dirige PRIMARY KEY(cod_equipo,cod_dt)
+);
+CREATE INDEX IXFK_Dirige_DirectoresTecnicos ON Dirige(cod_dt);
+CREATE INDEX IXFK_Dirige_Equipos ON Dirige(cod_equipo);
+
+/**
+Creación de la tabla de los jugadores de un equipo
+*/
+create table Jugadores(
+	cod_jugador decimal(12) not null,
+	cod_equipo varchar(4) not null,
+	cod_persona decimal(12),
+	participaciones_historico smallint,
+	goles_historico smallint,
+	partidos_ganados_historico smallint,
+	partidos_empatados_historico smallint,
+	partidos_perdidos_historico smallint,
+	cod_pais varchar (3),
+	
+	CONSTRAINT FK_Jugadores_Equipos FOREIGN KEY (cod_equipo) REFERENCES Equipos (cod_equipo),
+	CONSTRAINT FK_Jugadores_Personas FOREIGN KEY (cod_persona) REFERENCES Personas (cod_persona),
+	CONSTRAINT PK_Jugadores PRIMARY KEY(cod_jugador)
+);
+CREATE INDEX IXFK_Jugadores_Equipos ON Jugadores(cod_equipo);
+CREATE INDEX IXFK_Jugadores_Personas ON Jugadores(cod_persona);
+CREATE INDEX IXFK_Jugadores_Pais ON Jugadores(cod_pais);
